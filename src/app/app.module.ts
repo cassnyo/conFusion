64090f098cc121c6ab24020590585089b6ee3c4d
abelmonte <abelmonte@soax.es>
2019-01-31 13:13:57 +0100

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Importamos los tres nuevos modulos instalados en el proyecto
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    // Hemos de añadir los módulos a la sección de imports
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
