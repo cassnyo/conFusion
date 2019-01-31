599183f6d942c59c2bb743afb81066a5702205c4
abelmonte <abelmonte@soax.es>
2019-01-31 15:49:33 +0100

    Change list to grid-list and added a new card with the selected dish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule, MatButton} from '@angular/material/button'
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -21,7 +24,10 @@ import { MenuComponent } from './menu/menu.component';
     // Hemos de añadir los módulos a la sección de imports
     BrowserAnimationsModule, 
     MatToolbarModule,
-    MatListModule, 
+    MatListModule,  
+    MatGridListModule, 
+    MatCardModule, 
+    MatButtonModule,
     FlexLayoutModule
   ],
   providers: [],
