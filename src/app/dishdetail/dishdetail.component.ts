b0d6f70a6d912d1f1e6e897989c35c37c77e3cfb
abelmonte <abelmonte@soax.es>
2019-02-07 17:07:45 +0100

    RxJs - Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,8 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,6 +14,9 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: string[];
+  prev: string; // previous dish id
+  next: string; // next dish id
 
   constructor(
     private dishService: DishService,
@@ -20,13 +25,32 @@ export class DishdetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-      .subscribe((dish) => this.dish = dish);
+    // Get all dish ids
+    this.dishService.getDishIds()
+      .subscribe((dishIds) => { this.dishIds = dishIds; });
+
+      // Nos suscribirmos a los params del routing que ha cargado la página, si estos cambian
+      // (el dishId), se lanzará el observer y se refrescará el dish y el id previo/siguiente
+    this.route.params
+      .pipe(
+        switchMap((params) => this.dishService.getDish(params['id']))
+      )
+      .subscribe((dish) => {
+        this.dish = dish;
+        this.setPrevNext(dish.id);
+      });
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
     this.location.back();
   }
+
+
 
 }
