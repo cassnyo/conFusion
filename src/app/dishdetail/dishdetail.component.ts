e744e9417fe03cb51e9aafd3743cfa6f0ebe55b8
abelmonte <abelmonte@soax.es>
2019-02-25 14:33:38 +0100

    Angular Animations - Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,34 +7,20 @@ import {switchMap} from 'rxjs/operators';
 import {map} from 'rxjs/operators';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {Comment} from '../shared/comment';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {expand, flyInOut, visibility} from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block; '
+  },
   animations: [
-    // Detinimos un trigger de visibilidad. Este trigger tendrá dos estados, shwon y hidden
-    trigger('visibility', [
-        state(
-          'shown',
-          style({
-            transform: 'scale(1.0)',
-            opacity: 1
-          })
-        ),
-
-        state(
-          'hidden',
-          style({
-            transform: 'scale(0.5)',
-            opacity: 0
-          })
-        ),
-        // Ante cualquier cambio de estado (* => *), se hará una animación de 5 segundos
-        transition('* => *', animate('0.5s ease-in-out'))
-      ]
-    )
+    flyInOut(),
+    visibility(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
